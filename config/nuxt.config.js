85006b82f74005db24ef1e8085153c96947e3cb8
BichiKim <bichi@live.co.kr>
2018-04-12 18:02:39 +0900

    update

--- config/nuxt.config.js
+++ config/nuxt.config.js
@@ -1,10 +1,14 @@
 module.exports = {
+
   srcDir: './src',
+
   rootDir: './',
+
   /*
    ** Customize the progress bar color
    */
   loading: {color: '#3B8070'},
+
   /*
    ** Build configuration
    */
