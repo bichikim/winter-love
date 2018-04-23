b697ebae72f48ee1440e00774aa1a2f9138f75de
Bichi Kim <bichi@live.co.kr>
2018-04-24 01:44:00 +0900

    winterlove

--- .poetry/talent.ts
+++ .poetry/talent.ts
@@ -1,7 +1,7 @@
 /**
  *
- * We did not learn to be a good child when we were born and to be good lovers, neighbors, friends, parents, employees,
- * leaders and other roles. We can no longer be human unless we constantly reflect and learn.
+ * We did not learn to be a good child when we were born and to be a good lover, neighbor, friend, parent, employee,
+ * leader and other roles. We can no longer be human unless we constantly reflect and learn.
  * @author Bichi Kim <bichi@live.co.kr>
  */
 type TSpendingTime = () => {
