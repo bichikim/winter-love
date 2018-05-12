edccce0809544862c15fcbc63780f7f73e398dce
Bichi Kim <bichi@live.co.kr>
2018-05-12 22:44:25 +0900

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,6 +15,7 @@
   import AppLogo from '@/components/AppLogo.vue'
   import {Component, Vue} from 'vue-property-decorator'
   import {toggleFullScreen} from '~/full-screen'
+
   @Component({
     components: {
       AppLogo,
@@ -27,32 +28,36 @@
       toggleFullScreen(!this.isFullScreen)
       this.isFullScreen = !this.isFullScreen
     }
-    created(){
+
+    created() {
 
     }
   }
 </script>
 
 <style scoped lang="stylus">
-.container
-  min-height 100vh
-  display flex
-  justify-content center
-  align-items center
-  text-align center
-.title
-  font-family "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif /* 1 */
-  display block
-  font-weight 300
-  font-size 100px
-  color #35495e
-  letter-spacing 1px
-.subtitle
-  font-weight 300
-  font-size 42px
-  color #526488
-  word-spacing 5px
-  padding-bottom 15px
-.links
-  padding-top: 15px
+  .container
+    min-height 100vh
+    display flex
+    justify-content center
+    align-items center
+    text-align center
+
+  .title
+    font-family "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif /* 1 */
+    display block
+    font-weight 300
+    font-size 100px
+    color #35495e
+    letter-spacing 1px
+
+  .subtitle
+    font-weight 300
+    font-size 42px
+    color #526488
+    word-spacing 5px
+    padding-bottom 15px
+
+  .links
+    padding-top: 15px
 </style>
