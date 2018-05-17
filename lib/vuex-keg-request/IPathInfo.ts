25fa8239eded9bba895563a6289d00e0a5f7302f
BichiKim <bichi@live.co.kr>
2018-05-17 18:35:27 +0900

    update

--- lib/vuex-keg-request/IPathInfo.ts
+++ lib/vuex-keg-request/IPathInfo.ts
@@ -2,11 +2,11 @@ export const sParents = Symbol('pathInfo parent')
 export default interface IPathInfo {
   name?: string
   module?: {[name: string]: IPathInfo}
-  method?: string
+  method?: string | ((collection: any) => Promise<any>)
   basePath?: string
-  apiKey: string, // Todo: 파이어 베이스 키 ID 미구현
-  authDomain: string, // Todo: 파이어 베이스 도메인 미구현
-  projectId: string, // Todo: 파이어 스토어 ID 미구현
+  apiKey?: string, // Todo: 파이어 베이스 키 ID 미구현
+  authDomain?: string, // Todo: 파이어 베이스 도메인 미구현
+  projectId?: string, // Todo: 파이어 스토어 ID 미구현
   mode?: string // Todo: 어떤 요청으로 보낼지 설정 fireStore restful 기본값 restful
   path?: string | string[] | ((path: {[name: string]: string}) => string)
   inject?: { // auto inject
