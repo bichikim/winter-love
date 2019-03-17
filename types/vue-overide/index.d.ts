fe2c97589bad039aac32a81595f72c9a5de724b7
Bichi Kim <bichi@live.co.kr>
2019-03-17 23:13:12 +0900

    update

--- types/vue-overide/index.d.ts
+++ types/vue-overide/index.d.ts
@@ -1,12 +1,3 @@
-import {
-  DefaultComputed,
-  DefaultData,
-  DefaultMethods,
-  DefaultProps,
-  PropsDefinition,
-} from '@@/node_modules/vue/types/options'
-import {Vue} from '@@/node_modules/vue/types/vue'
-
 declare module '*.vue' {
   import {VueConstructor} from 'vue'
   const vue: VueConstructor
@@ -25,12 +16,7 @@ declare module 'vue/types/options' {
   import Vue from 'vue'
   // noinspection TsLint
   export interface ComponentOptions<
-    V extends Vue,
-    Data=DefaultData<V>,
-    Methods=DefaultMethods<V>,
-    Computed=DefaultComputed,
-    PropsDef=PropsDefinition<DefaultProps>,
-    Props=DefaultProps> {
+    V extends Vue>{
     middleware?: string
   }
 }
