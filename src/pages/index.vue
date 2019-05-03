25a448fd30f5818674d289b0a022b1cf7da21949
Bichi Kim <bichi@live.co.kr>
2019-05-04 01:49:04 +0900

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,23 +2,43 @@
   .main
     white-space {{text}}
     sm-container(:items="smData.items" :parent="smData")
+    q-nested-dragable(:items="nest")
 </template>
 
 <script lang="ts">
 import WhiteSpace from '@/components/WhiteSpace.vue'
 import SmContainer from '@/components/stateManagementPattern/SmContainer.vue'
 import SmContent from '@/components/stateManagementPattern/SmContent.vue'
+import QNestedDragable from '@/components/QNestedDragable.vue'
 import {ItemData} from '@/components/stateManagementPattern/types'
 import {
   Component, Vue, Prop,
 } from '~/vue-ts'
+import {Item} from '@/components/types/nested'
 
 @Component({
-  components: {WhiteSpace, SmContainer, SmContent},
+  components: {WhiteSpace, SmContainer, SmContent, QNestedDragable},
   layout: 'default',
 })
 export default class Index extends Vue {
   @Prop() test: string
+
+  nest: Item[] = [
+    {
+      id: '0',
+      name: 'item0',
+    },
+    {
+      id: '1',
+      name: 'item1',
+      items: [
+        {
+          id: '2',
+          name: 'item2',
+        },
+      ],
+    },
+  ]
 
 
   smData: ItemData = {
